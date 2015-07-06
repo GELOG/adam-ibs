1af7ac8e79d15bf9120d82889d344545052799f7
karim safraoui <karim.safraoui@percy-miller.com>
2015-07-06 15:05:11 -0400

    Refactorer CLI/Main

--- src/main/scala/com/ets/mgl804/core/Main.scala
+++ src/main/scala/com/ets/mgl804/core/Main.scala
@@ -2,6 +2,9 @@ package com.ets.mgl804.core
 
 import com.ets.mgl804.cli._
 import org.rogach.scallop._
+import org.slf4j.{LoggerFactory, Logger}
+import ch.qos.logback.classic.LoggerContext
+import ch.qos.logback.core.util.StatusPrinter
 
 /**
  * Created by ikizema on 15-05-30  : ivan.kizema at gmail.com
@@ -12,6 +15,10 @@ object Main {
   def main(args: Array[String]) {
 
     val conf: ListCommandsPlink = new ListCommandsPlink(args)
+
+    def logger = LoggerFactory.getLogger(this.getClass())
+
+    logger.info("vikas")
 
     conf.args(0) match {
         case "--file" => PlinkMethod.file(conf.file())
