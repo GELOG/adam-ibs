c12b912af342c745ac7549216d4b846080e45ecb
ikizema <kizema.ivan@gmail.com>
2015-07-09 13:09:40 -0400

    persistance customization

---
+++ adam-ibs-core/src/main/scala/com/ets/mgl804/core/AppContext.scala
@@ -0,0 +1,8 @@
+package com.ets.mgl804.core
+
+/**
+ * Created by ikizema on 15-07-03.
+ */
+class AppContext {
+
+}
---
+++ adam-ibs-core/src/main/scala/com/ets/mgl804/fonctions/PersistImportRecord.scala
@@ -0,0 +1,8 @@
+package com.ets.mgl804.fonctions
+
+/**
+ * Created by ikizema on 15-07-03.
+ */
+class PersistImportRecord {
+
+}
