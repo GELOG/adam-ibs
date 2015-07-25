e7636b417b29ab9093ff3f9c27db4f0d93e37c22
ikizema <kizema.ivan@gmail.com>
2015-07-25 11:49:30 -0400

    logback logger config

---
+++ adam-ibs-core/src/main/resources/logback.groovy
@@ -0,0 +1,15 @@
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.core.ConsoleAppender
+
+import static ch.qos.logback.classic.Level.DEBUG
+import static ch.qos.logback.classic.Level.INFO
+import static ch.qos.logback.classic.Level.WARN
+import static ch.qos.logback.classic.Level.ERROR
+
+appender("STDOUT", ConsoleAppender) {
+  encoder(PatternLayoutEncoder) {
+    pattern = "%d{HH:mm:ss.SSS} [%thread] [%-5level] [%logger{5}] : %msg%n"
+  }
+}
+
+root(INFO, ["STDOUT"])
