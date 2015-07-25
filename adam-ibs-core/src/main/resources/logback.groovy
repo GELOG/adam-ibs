1bc3400c791e327ab8808d014d4f938e614594f5
ikizema <kizema.ivan@gmail.com>
2015-07-25 12:13:16 -0400

    Configuration groovy

--- adam-ibs-core/src/main/resources/logback.groovy
+++ adam-ibs-core/src/main/resources/logback.groovy
@@ -5,11 +5,24 @@ import static ch.qos.logback.classic.Level.DEBUG
 import static ch.qos.logback.classic.Level.INFO
 import static ch.qos.logback.classic.Level.WARN
 import static ch.qos.logback.classic.Level.ERROR
+import static ch.qos.logback.classic.Level.OFF
 
 appender("STDOUT", ConsoleAppender) {
   encoder(PatternLayoutEncoder) {
-    pattern = "%d{HH:mm:ss.SSS} [%thread] [%-5level] [%logger{5}] : %msg%n"
+    pattern = "%d{HH:mm:ss.SSS} [%thread] [%-5level] [%logger{6}] : %msg%n"
+  }
+}
+//root(OFF, ["STDOUT"])
+logger("com.ets.mgl804", DEBUG, ["STDOUT"])
+logger("org.apache.spark", WARN, ["STDOUT"])
+
+def bySecond = timestamp("yyyyMMdd'T'HHmmss")
+appender("FILE", FileAppender) {
+  file = "./log/log-Adam-Ibs-${bySecond}.log"
+  append = true
+  encoder(PatternLayoutEncoder) {
+    pattern = "%level %logger - %msg%n"
   }
 }
 
-root(INFO, ["STDOUT"])
+logger("com.ets.mgl804", DEBUG, ["FILE"])
